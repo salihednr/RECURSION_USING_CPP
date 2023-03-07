18397108608afed7dd78c934396e6c397a332f50
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-03-07 11:36:38 +0530

    Create 01_Factorial.cpp

---
+++ EASY_PROBLEMS/01_Factorial.cpp
@@ -0,0 +1,16 @@
+/*
+Find The Factorial Of Number Using Recursion
+*/
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==0)
+        return 1;
+    return fact(n-1)*n;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
