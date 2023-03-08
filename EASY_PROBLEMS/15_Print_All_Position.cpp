8630b6c168819fb40b4254e9759dbf26dca627d0
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-03-08 12:00:35 +0530

    Create 15_Print_All_Position.cpp

---
+++ EASY_PROBLEMS/15_Print_All_Position.cpp
@@ -0,0 +1,20 @@
+/*
+Print All The Position Of Particular Element
+{5,5,6,5,2,7}
+5 = > 0 0 3
+*/
+#include<iostream>
+using namespace std;
+void printAllIndex(int ar,size,e,int i=0){
+    if(i==size)
+        return;
+    if(ar[i]==e)
+        cout<<i<<" ";
+    printAllIndex(ar,size,e,++i);
+}
+int main(){
+    int a[]={5,5,6,5,2,7};
+    int size=sizeof(a)/sizeof(int);
+    printAllIndex(a,size,5,0);
+    return 0;
+}
