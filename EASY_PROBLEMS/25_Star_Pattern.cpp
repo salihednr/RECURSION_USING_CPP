0f06e3b9c91023a8cd66e7948bf7656c9e815330
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-03-15 14:49:09 +0530

    Create 25_Star_Pattern.cpp

---
+++ EASY_PROBLEMS/25_Star_Pattern.cpp
@@ -0,0 +1,23 @@
+/*
+*****
+****
+***
+**
+*
+*/
+#include<iostream>
+using namespace std;
+void printStar(int n){
+    if(n==0)
+        return;
+    for(int i=0;i<n;i++)
+        cout<<"*";
+    cout<<endl;
+    printStar(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    printStar(n);
+    return 0;
+}
